50a27cc7ab72d76dc0e0a10c800a3ddb5aa9c425
bharath <bharath3366kumar@gmail.com>
2025-11-25 19:12:07 +0530

    second version

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
 a  =  7
 if  a% 2 == 0:
     print("even number")
-a  = 7
-if a% 2 == 0:
-    print("even number")
 else:
     print("odd number")
-num  =  input("enter a number")
-if  num% 2 == 0:
-    print("even number")
 
